4e5c5c288120ac8912d13df6a4a5901b47350894
NoverLord13 <spiridonov.ernest@gmail.com>
2026-06-09 16:51:48 +0900

    update header

---
+++ ykt_usligi-front/src/pages/AdAdder.tsx
@@ -0,0 +1,7 @@
+export const AdAdder = () => {
+  return (
+    <div className="p-6 max-w-7xl mx-auto text-slate-200 mt-6">
+      <h1 className="text-4xl font-bold text-black mb-2">Добавить объявление</h1>
+    </div>
+  );
+};
--- ykt_usligi-front/src/pages/Home.tsx
+++ ykt_usligi-front/src/pages/Home.tsx
@@ -3,10 +3,23 @@ import { Link, useNavigate } from 'react-router-dom';
 
 export const Home = () => {
 
-  return (
-    <div className="flex flex-col items-center w-full">
-        <h1 className="text-4xl font-bold text-white mt-10">Добро пожаловать в YKT Услуги!</h1>
-      
-    </div>
-  );
-};
+    const navigate = useNavigate();
+
+    return (
+        <div className="p-6 max-w-7xl mx-auto text-slate-200 mt-6">
+            <div className='flex justify-between items-center mb-8'>
+                <div>
+                    <h1 className="text-4xl font-bold text-black mb-2">Uslugi Ykt</h1>
+                </div>
+                
+                <button onClick={() => navigate('/adadder')} className="bg-indigo-600 hover:bg-indigo-500 text-white font-medium px-5 py-2.5 rounded-xl transition-all shadow-lg shadow-indigo-600/20">
+                    Добавить объявление
+                </button>
+            </div>
+
+            <div>
+                
+            </div>
+        </div>
+    );
+    };
