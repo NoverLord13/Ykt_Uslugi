4954dc596421866044c02638a5be62a22b3c45ac
ALP4ever <145176437+ALP4ever@users.noreply.github.com>
2026-06-10 11:56:43 +0900

    kaka

--- ykt_uslugi-back/main.py
+++ ykt_uslugi-back/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return "eshkere"
+
+
+if __name__== "__main__":
+    uvicorn.run("main:app", reload=True)
